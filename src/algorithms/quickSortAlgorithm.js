2705e935afa95ac66aeea822ae1baa651da1910b
fig781 <adeneilers@gmail.com>
2021-04-14 21:43:57 -0700

    quick sort working

--- src/algorithms/quickSortAlgorithm.js
+++ src/algorithms/quickSortAlgorithm.js
@@ -1,12 +1,13 @@
-// changeList.push({ index: r, value: arr[r], color: '#6ee7b7' });
-// changeList.push({ index: r, value: arr[r], color: '#ec4899' });
-// changeList.push({index: r, value: arr[r], color: '#a78bfa',});
-
 let changeList;
 
 const quickSortAlgorithm = (arr) => {
   changeList = [];
   partition(arr, 0, arr.length - 1);
+
+  for (let x = arr.length - 1; x >= 0; x--) {
+    changeList.push({ index: x, value: arr[x], color: '#a78bfa' });
+  }
+
   return changeList;
 };
 
@@ -18,36 +19,27 @@ const partition = (arr, low, high) => {
     tmp = arr[low];
     while (l < h) {
       while (arr[h] > tmp) {
-        changeList.push({ index: h, value: arr[h], color: '#6ee7b7' });
-        changeList.push({ index: h, value: arr[h], color: null });
         h--;
       }
-      arr[l] = arr[h];
       changeList.push({ index: l, value: arr[l], color: '#ec4899' });
+      arr[l] = arr[h];
+      changeList.push({ index: l, value: arr[l], color: '#6ee7b7' });
       changeList.push({ index: l, value: arr[l], color: null });
 
       while (tmp >= arr[l] && l < h) {
-        changeList.push({ index: l, value: arr[l], color: '#ec4899' });
-        changeList.push({ index: l, value: arr[l], color: null });
         l++;
       }
+      changeList.push({ index: l, value: arr[l], color: '#ec4899' });
       arr[h] = arr[l];
       changeList.push({ index: h, value: arr[h], color: '#6ee7b7' });
       changeList.push({ index: h, value: arr[h], color: null });
     }
     arr[l] = tmp;
-    changeList.push({ index: l, value: arr[l], color: '#6ee7b7' });
+    changeList.push({ index: l, value: arr[l], color: '#ec4899' });
     changeList.push({ index: l, value: arr[l], color: null });
     partition(arr, low, l - 1);
     low = l + 1;
   }
 };
-
-// //Need to wrap the function so it only accepts arr and only exports changeList
-// const exportFunction = (arr) => {
-//   changeList = [];
-//   quickSortAlgorithm(arr, 0, arr.length - 1);
-//   return changeList;
-// };
 
 export default quickSortAlgorithm;
