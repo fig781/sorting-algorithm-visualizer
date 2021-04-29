845f1c0224ad1d50a39fba1ca4316ea4be13a2d6
fig781 <adeneilers@gmail.com>
2021-04-28 21:45:18 -0700

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,44 @@
 /* eslint-disable no-restricted-globals */
-let CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = ['/', '/index.html'];
-self.addEventListener('install', function (event) {
-  // Perform install steps
+let CACHE_NAME = 'algo-sorter-cache';
+const urlsToCache = ['index.html'];
+
+//install SW
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(CACHE_NAME).then((cache) => {
       console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting();
+  //self.skipWaiting();
 });
 
-self.addEventListener('fetch', function (event) {
+//listen for requests
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
+    caches.match(event.request).then((response) => {
       if (response) {
         return response;
       }
       return fetch(event.request);
     })
+  );
+});
+
+//Activate the SW
+self.addEventListener('activate', (event) => {
+  const cacheWhiteList = [];
+  cacheWhiteList.push(CACHE_NAME);
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhiteList.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
   );
 });
